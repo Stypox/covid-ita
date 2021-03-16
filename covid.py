e332a38c443eb4b20820af8364103aca3305eb65
Stypox <stypox@pm.me>
2021-03-16 21:32:07 +0100

    Extract plot function

--- covid.py
+++ covid.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 import time
 import datetime
 
-JSON_ITA_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
-JSON_VACCINI_ITA_URL = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.json"
-JSON_REGIONI_ITA_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json"
+JSON_ITALIA_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
+JSON_VACCINI_URL = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.json"
+JSON_REGIONI_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json"
 
 def dateStringToObject(dateString):
 	return datetime.date(int(dateString[:4]), int(dateString[5:7]), int(dateString[8:10]))
 
 class DataRegione:
-	def __init__(self, startingDate, arrayLength, code, name):
+	def __init__(self, firstDay, dayCount, code, name):
 		def buildArray():
-			return [0 for i in range(arrayLength)]
+			return [0 for i in range(dayCount)]
 
-		self.startingDate = startingDate
+		self.firstDay = firstDay
 		self.code = code
 		self.name = name
 
@@ -33,65 +33,76 @@ class DataRegione:
 		self.tamponi = buildArray()
 		self.casi_testati = buildArray()
 		self.ingressi_terapia_intensiva = buildArray()
+		self.nuovi_vaccini = buildArray()
 
 	def addDataPoint(self, jsonObject):
 		date = dateStringToObject(jsonObject["data"])
-		i = (date - self.startingDate).days
+		i = (date - self.firstDay).days
 		for key, value in jsonObject.items():
 			if hasattr(self, key):
 				getattr(self, key)[i] = (0 if value is None else int(value))
 
+	def addVaccino(self, jsonObject):
+		date = dateStringToObject(jsonObject["data_somministrazione"])
+		i = (date - self.firstDay).days
+		self.nuovi_vaccini[i] += jsonObject["totale"]
+
+	def trim(self):
+		# TODO non togliere zero reali in fondo
+		self.ricoverati_con_sintomi = np.trim_zeros(self.ricoverati_con_sintomi, 'b')
+		self.terapia_intensiva = np.trim_zeros(self.terapia_intensiva, 'b')
+		self.totale_ospedalizzati = np.trim_zeros(self.totale_ospedalizzati, 'b')
+		self.isolamento_domiciliare = np.trim_zeros(self.isolamento_domiciliare, 'b')
+		self.totale_positivi = np.trim_zeros(self.totale_positivi, 'b')
+		self.variazione_totale_positivi = np.trim_zeros(self.variazione_totale_positivi, 'b')
+		self.nuovi_positivi = np.trim_zeros(self.nuovi_positivi, 'b')
+		self.dimessi_guariti = np.trim_zeros(self.dimessi_guariti, 'b')
+		self.deceduti = np.trim_zeros(self.deceduti, 'b')
+		self.totale_casi = np.trim_zeros(self.totale_casi, 'b')
+		self.tamponi = np.trim_zeros(self.tamponi, 'b')
+		self.casi_testati = np.trim_zeros(self.casi_testati, 'b')
+		self.ingressi_terapia_intensiva = np.trim_zeros(self.ingressi_terapia_intensiva, 'b')
+		self.nuovi_vaccini = np.trim_zeros(self.nuovi_vaccini, 'b')
+
 
 class Data:
 	def __init__(self):
-		json = requests.get(JSON_ITA_URL).json()
-		def buildArray(key):
-			return [0 if e[key] is None else int(e[key]) for e in json]
+		jsonItalia = requests.get(JSON_ITALIA_URL).json()
+		self.date = [dateStringToObject(e["data"]) for e in jsonItalia]
+		firstDay = self.date[0]
+		dayCount = len(jsonItalia)
+		self.numero_giorni = dayCount
 
-		self.date = [dateStringToObject(e["data"]) for e in json]
-		self.ricoverati_con_sintomi = buildArray("ricoverati_con_sintomi")
-		self.terapia_intensiva = buildArray("terapia_intensiva")
-		self.totale_ospedalizzati = buildArray("totale_ospedalizzati")
-		self.isolamento_domiciliare = buildArray("isolamento_domiciliare")
-		self.totale_positivi = buildArray("totale_positivi")
-		self.variazione_totale_positivi = buildArray("variazione_totale_positivi")
-		self.nuovi_positivi = buildArray("nuovi_positivi")
-		self.dimessi_guariti = buildArray("dimessi_guariti")
-		self.deceduti = buildArray("deceduti")
-		self.totale_casi = buildArray("totale_casi")
-		self.tamponi = buildArray("tamponi")
-		self.casi_testati = buildArray("casi_testati")
-		self.ingressi_terapia_intensiva = buildArray("ingressi_terapia_intensiva")
-
-		jsonVaccini = requests.get(JSON_VACCINI_ITA_URL).json()["data"]
-		self.nuovi_vaccini = [0 for _ in range(len(json))]
+		self.italia = DataRegione(firstDay, dayCount, -1, "Tutta Italia")
+		for dataPoint in jsonItalia:
+			self.italia.addDataPoint(dataPoint)
+		jsonVaccini = requests.get(JSON_VACCINI_URL).json()["data"]
 		for dataPoint in jsonVaccini:
-			dateSomministrazione = dateStringToObject(dataPoint["data_somministrazione"])
-			self.nuovi_vaccini[(dateSomministrazione - self.date[0]).days] += dataPoint["totale"]
-		self.nuovi_vaccini = np.trim_zeros(self.nuovi_vaccini, 'b') # remove zeros at end
+			self.italia.addVaccino(dataPoint)
+		self.italia.trim()
 
-		jsonRegioni = requests.get(JSON_REGIONI_ITA_URL).json()
-		self.piemonte = DataRegione(self.date[0], len(json), 1, "Piemonte")
-		self.valle_d_aosta = DataRegione(self.date[0], len(json), 2, "Valle d'Aosta")
-		self.lombardia = DataRegione(self.date[0], len(json), 3, "Lombardia")
-		self.veneto = DataRegione(self.date[0], len(json), 5, "Veneto")
-		self.friuli_venezia_giulia = DataRegione(self.date[0], len(json), 6, "Friuli Venezia Giulia")
-		self.liguria = DataRegione(self.date[0], len(json), 7, "Liguria")
-		self.emilia_romagna = DataRegione(self.date[0], len(json), 8, "Emilia Romagna")
-		self.toscana = DataRegione(self.date[0], len(json), 9, "Toscana")
-		self.umbria = DataRegione(self.date[0], len(json), 10, "Umbria")
-		self.marche = DataRegione(self.date[0], len(json), 11, "Marche")
-		self.lazio = DataRegione(self.date[0], len(json), 12, "Lazio")
-		self.abruzzo = DataRegione(self.date[0], len(json), 13, "Abruzzo")
-		self.molise = DataRegione(self.date[0], len(json), 14, "Molise")
-		self.campania = DataRegione(self.date[0], len(json), 15, "Campania")
-		self.puglia = DataRegione(self.date[0], len(json), 16, "Puglia")
-		self.basilicata = DataRegione(self.date[0], len(json), 17, "Basilicata")
-		self.calabria = DataRegione(self.date[0], len(json), 18, "Calabria")
-		self.sicilia = DataRegione(self.date[0], len(json), 19, "Sicilia")
-		self.sardegna = DataRegione(self.date[0], len(json), 20, "Sardegna")
-		self.alto_adige = DataRegione(self.date[0], len(json), 21, "Alto Adige")
-		self.trentino = DataRegione(self.date[0], len(json), 22, "Trentino")
+		jsonRegioni = requests.get(JSON_REGIONI_URL).json()
+		self.piemonte = DataRegione(firstDay, dayCount, 1, "Piemonte")
+		self.valle_d_aosta = DataRegione(firstDay, dayCount, 2, "Valle d'Aosta")
+		self.lombardia = DataRegione(firstDay, dayCount, 3, "Lombardia")
+		self.veneto = DataRegione(firstDay, dayCount, 5, "Veneto")
+		self.friuli_venezia_giulia = DataRegione(firstDay, dayCount, 6, "Friuli Venezia Giulia")
+		self.liguria = DataRegione(firstDay, dayCount, 7, "Liguria")
+		self.emilia_romagna = DataRegione(firstDay, dayCount, 8, "Emilia Romagna")
+		self.toscana = DataRegione(firstDay, dayCount, 9, "Toscana")
+		self.umbria = DataRegione(firstDay, dayCount, 10, "Umbria")
+		self.marche = DataRegione(firstDay, dayCount, 11, "Marche")
+		self.lazio = DataRegione(firstDay, dayCount, 12, "Lazio")
+		self.abruzzo = DataRegione(firstDay, dayCount, 13, "Abruzzo")
+		self.molise = DataRegione(firstDay, dayCount, 14, "Molise")
+		self.campania = DataRegione(firstDay, dayCount, 15, "Campania")
+		self.puglia = DataRegione(firstDay, dayCount, 16, "Puglia")
+		self.basilicata = DataRegione(firstDay, dayCount, 17, "Basilicata")
+		self.calabria = DataRegione(firstDay, dayCount, 18, "Calabria")
+		self.sicilia = DataRegione(firstDay, dayCount, 19, "Sicilia")
+		self.sardegna = DataRegione(firstDay, dayCount, 20, "Sardegna")
+		self.alto_adige = DataRegione(firstDay, dayCount, 21, "Alto Adige")
+		self.trentino = DataRegione(firstDay, dayCount, 22, "Trentino")
 		self.regioni = [self.piemonte, self.valle_d_aosta, self.lombardia, self.veneto, self.friuli_venezia_giulia, self.liguria,
 			self.emilia_romagna, self.toscana, self.umbria, self.marche, self.lazio, self.abruzzo, self.abruzzo, self.molise,
 			self.campania, self.puglia, self.basilicata, self.calabria, self.sicilia, self.sardegna, self.alto_adige, self.trentino]
@@ -99,6 +110,8 @@ class Data:
 			for regione in self.regioni:
 				if regione.code == dataPoint["codice_regione"]:
 					regione.addDataPoint(dataPoint)
+		for regione in self.regioni:
+			regione.trim()
 
 
 def incremento(arr):
@@ -115,29 +128,31 @@ def mediaMobile(arr):
 	return mm
 
 
+
 def plotConMediaMobile(arr, colore):
 	mm = mediaMobile(arr)
-	plt.plot_date(data.date[0:len(arr)], arr, linewidth=0.5, color=colore+"77", fmt="b-")
-	plt.plot_date(data.date[0:len(mm)], mm, linewidth=1.2, color=colore+"ff", fmt="b-")
+	plt.plot_date(d.date[0:len(arr)], arr, linewidth=0.5, color=colore+"77", fmt="b-")
+	plt.plot_date(d.date[0:len(mm)], mm, linewidth=1.2, color=colore+"ff", fmt="b-")
 
 def plotLineaZero(colore):
-	plt.plot_date(data.date[0:len(data.nuovi_positivi)], [0 for i in range(len(data.nuovi_positivi))], linewidth=0.5, color=colore+"77", fmt="b-")
+	plt.plot_date(d.date[0:d.numero_giorni], [0 for i in range(d.numero_giorni)], linewidth=0.5, color=colore+"77", fmt="b-")
 
-def plotPercentualePositivi(colore):
-	incrementoTamponi = incremento(data.tamponi)
+def plotPercentualePositivi(regione, colore):
+	incrementoTamponi = incremento(regione.tamponi)
 	incrementoTamponi[0] = incrementoTamponi[1]
 	incrementoTamponi[297] = (incrementoTamponi[296] + incrementoTamponi[298]) / 2
-	plotConMediaMobile(np.multiply(np.divide(data.nuovi_positivi, incrementoTamponi), 50000), colore)
+	plotConMediaMobile(np.multiply(np.divide(regione.nuovi_positivi, incrementoTamponi), 50000), colore)
 
+def plot(regione):
+	plotConMediaMobile(regione.nuovi_positivi, "#ff0000")
+	plotConMediaMobile(incremento(regione.nuovi_positivi), "#bb0000")
+	plotPercentualePositivi(regione, "#aaaa00")
+	plotConMediaMobile(regione.totale_ospedalizzati, "#0000ff")
+	plotConMediaMobile(incremento(regione.totale_ospedalizzati), "#0000bb")
+	plotConMediaMobile(regione.nuovi_vaccini, "#00ff00")
 
-data = Data()
-plotConMediaMobile(data.trentino.nuovi_positivi, "#ff0000")
-plotConMediaMobile(incremento(data.trentino.nuovi_positivi), "#bb0000")
-plotPercentualePositivi("#aaaa00")
-plotConMediaMobile(data.totale_ospedalizzati, "#0000ff")
-plotConMediaMobile(incremento(data.totale_ospedalizzati), "#0000bb")
-plotConMediaMobile(data.nuovi_vaccini, "#00ff00")
+d = Data()
+plot(d.italia)
 plotLineaZero("#000000")
-
 plt.xticks(rotation=90)
 plt.show()
